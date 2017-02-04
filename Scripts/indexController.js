ab146ee7ae549be993812c2d9472d4351ac81167
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-04 03:40:14 +0200

    add indexController.js

---
+++ Scripts/indexController.js
@@ -0,0 +1,13 @@
+(function () {
+    var app = angular.module('app');
+
+    app.controller('indexController',function($scope,ipService){
+        var init = function(){
+            ipService.getIp().then(function(response){
+                $scope.location = response.data;
+            },function(err){
+                console.log(err.statusText);
+            });
+        };
+    });
+}());
