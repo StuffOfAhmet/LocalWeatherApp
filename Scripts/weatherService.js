be994a8bad8ea934e686666ed150570e06c8712f
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-04 04:44:48 +0200

    complete weatherService

--- Scripts/weatherService.js
+++ Scripts/weatherService.js
@@ -1,12 +1,13 @@
-(function(){
+(function () {
     var app = angular.module('app');
-    
-    app.factory('weatherService',function($http){
+
+    app.factory('weatherService', function ($http) {
         var appId = '41ee98a9067f4ad60aee8195e101027d';
-        
+
         return {
-            getWeather:function(){
-                return $http.get('http://ipinfo.io');
+            getWeather: function (city, country) {
+                var api = 'http://api.openweathermap.org/data/2.5/weather?q=' + city + ',' + country + '&appid=' + appId;
+                return $http.get(api);
             }
         };
     });
